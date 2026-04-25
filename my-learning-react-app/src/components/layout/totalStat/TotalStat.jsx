122ee100e5b1c4c4d227d732ebbb8cf048ed4f1e
Rhito <lasnguyen06@gmail.com>
2026-04-25 11:54:23 +0700

    feat: update total statistics to reflect dynamic values from context

--- my-learning-react-app/src/components/layout/totalStat/TotalStat.jsx
+++ my-learning-react-app/src/components/layout/totalStat/TotalStat.jsx
@@ -4,34 +4,42 @@ import purseImg from "./../../../asset/purse.png";
 import cartImg from "./../../../asset/cart.png";
 import packageImg from "./../../../asset/package.png";
 import walletImg from "./../../../asset/wallet.png";
+import { useContext } from "react";
+import { ProductContext } from "../../../context/ProductContext";
+import { CartContext } from "../../../context/CartContext";
+
 export default function TotalStat() {
+  const { products } = useContext(ProductContext);
+
+  const { cart, totalQuantity, totalPay } = useContext(CartContext);
+
   const statList = [
     {
       id: 1,
       src: purseImg,
       title: "Tổng sản phẩm",
-      totalAmount: 0,
+      totalAmount: products.length,
       description: "Sản phẩm hệ thống",
     },
     {
       id: 2,
       src: cartImg,
       title: "Sản phẩm trong giỏ",
-      totalAmount: 0,
+      totalAmount: cart.length,
       description: "Sản phẩm đã chọn",
     },
     {
       id: 3,
       src: packageImg,
       title: "Tổng số lượng",
-      totalAmount: 0,
+      totalAmount: totalQuantity,
       description: "Tổng số lượng sản phẩm",
     },
     {
       id: 4,
       src: walletImg,
       title: "Tổng tiền",
-      totalAmount: 0,
+      totalAmount: totalPay,
       description: "Tổng giá trị giỏ hàng",
     },
   ];
